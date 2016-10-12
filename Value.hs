9e68954907ba1bf03509d2645adddab70313135a
miltongneto <mvgn@cin.ufpe.br>
2016-10-12 16:51:02 -0300

    instance Eq Value implementado

--- Value.hs
+++ Value.hs
@@ -17,6 +17,14 @@ instance Show Value where
   show (String str) = "\"" ++ str ++ "\""
   show (Var name) = name
   show Nil = "undefined"
+
+instance Eq Value where
+  (Bool b1) == (Bool b2) = b1 == b2
+  (Int x) == (Int y) = x == y 
+  (String str1) == (String str2) = str1 == str2
+  (Var x) == (Var y) = x == y
+  Nil == Nil = True
+  _ == _ = False
   
 -- This function could be replaced by (unwords.map show). The unwords
 -- function takes a list of String values and uses them to build a 
